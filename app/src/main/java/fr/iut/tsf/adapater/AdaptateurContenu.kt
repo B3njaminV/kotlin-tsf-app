4a1e0227573273e4c15f35239044bdc8944e96d4
Benjamin <Benjamin.valleix@gmail.com>
2023-01-16 22:11:55 +0100

    Ajout adaptateur

---
+++ app/src/main/java/fr/iut/tsf/adapater/AdaptateurContenu.kt
@@ -0,0 +1,12 @@
+package fr.iut.tsf.adapater
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import fr.iut.tsf.R
+
+class AdaptateurContenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.adaptateur_contenu)
+    }
+}
