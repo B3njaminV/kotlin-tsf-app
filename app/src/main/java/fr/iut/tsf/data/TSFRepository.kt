122ee83ca9b9c0eb5384400c7d655f945ef3807f
Benjamin <Benjamin.valleix@gmail.com>
2023-01-31 11:15:08 +0100

    Ajout entité

--- app/src/main/java/fr/iut/tsf/data/TSFRepository.kt
+++ app/src/main/java/fr/iut/tsf/data/TSFRepository.kt
@@ -2,21 +2,22 @@ package fr.iut.tsf.data
 
 import androidx.annotation.WorkerThread
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.asLiveData
+import fr.iut.tsf.api.TSFManager
 import fr.iut.tsf.model.Film
 import kotlinx.coroutines.flow.Flow
 
-class TSFRepository(private val tsfDAO: TSFDao){
+class TSFRepository(private val tsfDAO: TSFDao, private val TSFManager: TSFManager){
 
-    val allMovie: LiveData<List<Film>> = tsfDAO.getAll().asLiveData()
+    private val movies = tsfDAO.getAll().asLiveData()
 
-    // By default Room runs suspend queries off the main thread, therefore, we don't need to
-    // implement anything else to ensure we're not doing long running database work
-    // off the main thread.
-    @Suppress("RedundantSuspendModifier")
-    @WorkerThread
-    suspend fun insert(film: Film) {
-        tsfDAO.insert(film)
+    val allMovies: LiveData<List<Film>> = Transformations.map(movies) { entities ->
+        val list = mutableListOf<Film>()
+        for (item: Film in entities) {
+            list.add(Film(item.id, /*item.date,*/ item.duree))
+        }
+
+        list
     }
-
 }
--- app/src/main/java/fr/iut/tsf/model/Film.kt
+++ app/src/main/java/fr/iut/tsf/model/Film.kt
@@ -1,7 +1,4 @@
 package fr.iut.tsf.model
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
 
-@Entity(tableName = "film")
-data class Film(@PrimaryKey(autoGenerate = true) val id: Int, val nom: String, val path: String, val note: Double)
+data class Film(val id: Int, val nom: String, val path: String, val note: Double)
